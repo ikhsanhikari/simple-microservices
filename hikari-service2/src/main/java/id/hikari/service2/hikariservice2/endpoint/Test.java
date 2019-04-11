48b3f28faf7fa865c5d26b42b34d4e67bf8fcca1
Nurikhsan <ikhsanhikari29@gmail.com>
2019-04-12 03:18:22 +0700

    init

--- hikari-service2/src/main/java/id/hikari/service2/hikariservice2/endpoint/Test.java
+++ hikari-service2/src/main/java/id/hikari/service2/hikariservice2/endpoint/Test.java
@@ -1,10 +1,12 @@
 package id.hikari.service2.hikariservice2.endpoint;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
+    @RequestMapping("/")
     public String test(){
-        return "Please Work !!!!!!!!";
+        return "Please Work Hikari 2 !!!!!!!!";
     }
 }
